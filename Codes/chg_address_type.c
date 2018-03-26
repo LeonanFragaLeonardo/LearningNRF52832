912e8e18214e53ffd49256f9c1bcb686e3b6b2e3
LeonanFragaLeonardo <leonan@alunos.utfpr.edu.br>
2018-03-26 14:25:45 +0100

    Update chg_address_type.c

--- Codes/chg_address_type.c
+++ Codes/chg_address_type.c
@@ -2,9 +2,9 @@ static void gap_params_init(void){
   ...
 //Esse trecho vai alterar o tipo de endereço para Private resolvable Address. O qual é aleatório, no entanto a cada "reset" um endereço diferente é gerado.
 		
-		ble_gap_addr_t gap_address;
-		gap_address.addr_type = BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_RESOLVABLE, &gap_address;
-		err_code = sd_ble_gap_address_set(BLE_GAP_ADDR_CYCLE_MODE_AYTO, &gap_address);
-		
-		APP_ERROR_CHECK(err_code);
+	ble_gap_addr_t gap_address;
+	gap_address.addr_type = BLE_GAP_ADDR_TYPE_RANDOM_PRIVATE_RESOLVABLE, &gap_address;
+	err_code = sd_ble_gap_address_set(BLE_GAP_ADDR_CYCLE_MODE_AYTO, &gap_address);
+
+	APP_ERROR_CHECK(err_code);
 }
